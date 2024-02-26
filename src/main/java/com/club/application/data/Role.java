3e6e90a932b1e8e1adc3aa1b6d1e151b79e0103d
start.vaadin.com <no-reply@vaadin.com>
2024-02-26 08:04:41 +0000

    Generated project

---
+++ src/main/java/com/club/application/data/Role.java
@@ -0,0 +1,5 @@
+package com.club.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/com/club/application/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.club.application.services;
+
+import org.springframework.lang.NonNullApi;
